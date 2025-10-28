6a95ef46854170fa1fdd4d47cb3da3e59f52af8d
José Bieco <jo_bieco@hotmail.com>
2025-10-28 18:50:25 -0300

    refactor(src/algoritmo/metodos/MILP/MILP.ts): refatorada a classe resposável por lidar com o modelo matemático e chamar o solver.

--- src/algoritmo/metodos/MILP/MILP.ts
+++ src/algoritmo/metodos/MILP/MILP.ts
@@ -1,8 +1,14 @@
 import Constraint from "@/algoritmo/abstractions/Constraint";
-import { OptimizationModel, Term, Variable } from "./optimization_model";
+import { OptimizationModel, Variable } from "./optimization_model";
 import { ObjectiveFunction } from "@/algoritmo/classes/ObjectiveFunction";
 
-import { LpSum } from "./utils";
+import { ExactAlgorithm } from "@/algoritmo/abstractions/ExactAlgorithm";
+import {
+  Context,
+  HighsSolverResult,
+  Solucao,
+} from "@/algoritmo/communs/interfaces/interfaces";
+import { ObjectiveComponent } from "@/algoritmo/abstractions/ObjectiveComponent";
 
 export interface modelSets {
   D: number[];
@@ -20,6 +26,7 @@ export interface modelConsts {
   P: (0 | 1)[][];
   omega: (1 | 0.75)[];
   eta: (1 | 0.75)[];
+  Pmax: number;
 }
 
 export interface modelParamns {
@@ -28,11 +35,12 @@ export interface modelParamns {
   v: (Variable | null)[][][];
   z: Variable[];
   w: Variable[];
+  b: Variable[][]; // (3) flexível
 }
 
 export type modelSCP = modelSets & modelConsts & modelParamns;
 
-export class MILP {
+export class MILP extends ExactAlgorithm {
   public name: string;
 
   public model: OptimizationModel;
@@ -44,7 +52,13 @@ export class MILP {
 
   public modelSets: modelSets;
 
-  public modelConsts: modelConsts = { BigM: 0, eta: [], omega: [], P: [] };
+  public modelConsts: modelConsts = {
+    BigM: 0,
+    eta: [],
+    omega: [],
+    P: [],
+    Pmax: 0,
+  };
 
   public modelParamns: modelParamns = {
     x: undefined,
@@ -52,11 +66,34 @@ export class MILP {
     v: undefined,
     w: undefined,
     z: undefined,
+    b: undefined,
   };
 
   public objectiveFunction: ObjectiveFunction;
 
-  constructor(name: string, modelSets: modelSets) {
+  constructor(
+    name: string,
+    context: Context,
+    constraints: Constraint<any>[],
+    solution: Solucao | undefined,
+    objectiveType: "min" | "max",
+    objectiveComponentes: ObjectiveComponent[],
+    maiorPrioridade: number | undefined,
+    enableStatistics: boolean,
+
+    modelSets: modelSets
+  ) {
+    super(
+      name,
+      context,
+      constraints,
+      solution,
+      objectiveType,
+      objectiveComponentes,
+      maiorPrioridade,
+      enableStatistics
+    );
+
     this.name = name;
 
     this.modelSets = modelSets;
@@ -64,9 +101,6 @@ export class MILP {
     this.model = new OptimizationModel(name);
 
     // --- Cálculo de Parâmetros Derivados ---
-    this.modelConsts.P = this.modelSets.D.map((i) =>
-      this.modelSets.T.map((j) => (this.modelSets.p[i][j] > 0 ? 1 : 0))
-    );
     this.modelConsts.BigM = this.modelSets.c.reduce((sum, val) => sum + val, 0);
     this.modelConsts.omega = this.modelSets.D.map((i) =>
       this.modelSets.s[i] > 2 ? 0.75 : 1
@@ -75,10 +109,28 @@ export class MILP {
       this.modelSets.s[i] < -1 ? 0.75 : 1
     );
 
+    this.modelConsts.Pmax = Math.max(...this.modelSets.p.flat()) + 1;
+
+    this.modelConsts.P = this.modelSets.D.map((i) =>
+      this.modelSets.T.map(
+        (j) => (this.modelSets.p[i][j] > 0 ? 1 : 0)
+        // this.modelSets.p[i][j] !== this.modelConsts.Pmax - 1 ? 1 : 0
+      )
+    );
+
     // --- Criação das Variáveis de Decisão ---
     this.modelParamns.x = this.modelSets.D.map((i) =>
       this.modelSets.T.map((j) =>
         this.model.addVariable(`x_${i}_${j}`, { type: "Binary" })
+      )
+    );
+
+    /**
+     * Utilizado caso a restrição (3) atribuição somente com formulário ou trava, seja dada como flexível
+     */
+    this.modelParamns.b = this.modelSets.D.map((i) =>
+      this.modelSets.T.map((j) =>
+        this.model.addVariable(`b_${i}_${j}`, { type: "Binary" })
       )
     );
 
@@ -106,126 +158,82 @@ export class MILP {
     this.modelParamns.w = this.modelSets.D.map((i) =>
       this.model.addVariable(`w_${i}`, { type: "Continuous", lb: 0 })
     );
-    // --- Fim Criação das Variáveis de Decisão ---
-
-    const L_sup = 2;
-    const L_inf = 1;
-    // 3. Multiplicadores da Função Objetivo
-    const K = { K1: 1000, K2: 1000000, K3: 100000, K4: 10000, K5: 10000 };
-
-    const Pmax = Math.max(...this.modelSets.p.flat()) + 1;
-
-    // --- Definição da Função Objetivo ---
-    const objectiveTerms: Term[] = [];
-    this.modelSets.D.forEach((i) =>
-      this.modelSets.T.forEach((j) => {
-        if (this.modelSets.p[i][j] > 0)
-          objectiveTerms.push({
-            variable: this.modelParamns.x[i][j],
-            coefficient: K.K1 * (Pmax - this.modelSets.p[i][j]),
-          });
-      })
-    );
-
-    this.modelParamns.u.forEach((u_j) =>
-      objectiveTerms.push({ variable: u_j, coefficient: -K.K2 })
-    );
-    this.modelSets.D.forEach((i) =>
-      this.modelSets.F.forEach(([j, k]) => {
-        const v_ijk = this.modelParamns.v[i][j][k];
-        if (v_ijk) objectiveTerms.push({ variable: v_ijk, coefficient: -K.K3 });
-      })
-    );
-    this.modelParamns.z.forEach((z_i, i) =>
-      objectiveTerms.push({
-        variable: z_i,
-        coefficient: -K.K4 * this.modelConsts.omega[i],
-      })
-    );
-    this.modelParamns.w.forEach((w_i, i) =>
-      objectiveTerms.push({
-        variable: w_i,
-        coefficient: -K.K5 * this.modelConsts.eta[i],
-      })
-    );
-    this.model.setObjective("maximize", LpSum(objectiveTerms));
-
-    this.modelSets.D.forEach((i) =>
-      this.modelSets.T.forEach((j) => {
-        this.model.addConstraint(
-          `prioridade_definida_${i}_${j}`,
-          LpSum([this.modelParamns.x[i][j]]),
-          "<=",
-          this.modelConsts.P[i][j] + this.modelSets.m[i][j]
-        );
-      })
-    );
-    this.modelSets.D.forEach((i) =>
-      this.modelSets.T.forEach((j) => {
-        if (this.modelSets.m[i][j] == 1) {
-          this.model.addConstraint(
-            `trava_${i}_${j}`,
-            LpSum([this.modelParamns.x[i][j]]),
-            "==",
-            this.modelSets.a[i][j]
-          );
-        }
-      })
-    );
-
-    this.modelSets.T.forEach((j) => {
-      const lhs = this.modelSets.D.map((i) => this.modelParamns.x[i][j]);
-      lhs.push(this.modelParamns.u[j]);
-      this.model.addConstraint(`cobertura_turma_${j}`, LpSum(lhs), "==", 1);
-    });
-
-    this.modelSets.D.forEach((i) =>
-      this.modelSets.F.forEach(([j, k]) => {
-        const v_ijk = this.modelParamns.v[i][j][k]!;
-        const lhs = LpSum([
-          { variable: this.modelParamns.x[i][j], coefficient: 1 },
-          { variable: this.modelParamns.x[i][k], coefficient: 1 },
-          { variable: v_ijk, coefficient: -1 },
-        ]);
-        this.model.addConstraint(
-          `conflito_horario_${i}_${j}_${k}`,
-          lhs,
-          "<=",
-          1
-        );
-      })
-    );
-
-    // (8) Carga de trabalho Mínima (Restrição Flexível)
-    this.modelSets.D.forEach((i) => {
-      const terms = this.modelSets.T.map((j) => ({
-        variable: this.modelParamns.x[i][j],
-        coefficient: this.modelSets.c[j],
-      }));
-      terms.push({
-        variable: this.modelParamns.z[i],
-        coefficient: this.modelConsts.BigM,
-      });
-      this.model.addConstraint(`carga_minima_${i}`, LpSum(terms), ">=", L_inf);
-    });
-
-    // (9) Carga de trabalho Máxima (Restrição Flexível)
-    this.modelSets.D.forEach((i) => {
-      // Cria o somatório: Σ c_j * x_i,j
-      const terms = this.modelSets.T.map((j) => ({
-        variable: this.modelParamns.x[i][j],
-        coefficient: this.modelSets.c[j],
-      }));
-
-      // Adiciona o termo -w_i (antes era -BigM * w_i)
-      terms.push({ variable: this.modelParamns.w[i], coefficient: -1 });
-
-      // Adiciona a restrição: (Σ c_j * x_i,j) - w_i <= L_sup
-      this.model.addConstraint(`carga_maxima_${i}`, LpSum(terms), "<=", L_sup);
-    });
   }
 
-  async execute() {
+  /**
+   * Implementação do método abstrato de ExactAlgorithm.
+   * Itera sobre os componentes e restrições e os adiciona ao modelo.
+   */
+  protected buildModel(): void {
+    // 1. Define o sentido da otimização
+    this.model.setObjectiveSense(
+      this.objectiveFunction.type === "min" ? "minimize" : "maximize"
+    );
+
+    // 2. Adiciona Componentes da Função Objetivo
+    for (const component of this.objectiveFunction.components.values()) {
+      if (component.isActive && component.milpFormulation) {
+        const terms = component.milpFormulation(this.model, {
+          ...this.modelSets,
+          ...this.modelConsts,
+          ...this.modelParamns,
+        });
+
+        // Lógica para inverter o sinal se o tipo do componente for
+        // oposto ao da função objetivo principal
+        const multiplier =
+          component.type === this.objectiveFunction.type ? 1 : -1;
+
+        const finalTerms = terms.map((t) => ({
+          ...t,
+          coefficient: t.coefficient * multiplier,
+          // coefficient: t.coefficient * component.multiplier * multiplier,
+        }));
+
+        this.model.addObjectiveTerms(finalTerms);
+      }
+    }
+
+    // 3. Adiciona Restrições Hard
+    for (const constraint of this.constraints.hard.values()) {
+      if (constraint.isActive && constraint.milpHardFormulation) {
+        constraint.milpHardFormulation(this.model, {
+          ...this.modelSets,
+          ...this.modelConsts,
+          ...this.modelParamns,
+        });
+      }
+    }
+
+    // 4. Adiciona Restrições Soft (como penalidades no objetivo)
+    for (const constraint of this.constraints.soft.values()) {
+      if (constraint.isActive && constraint.milpSoftFormulation) {
+        const { objectiveTerms } = constraint.milpSoftFormulation(this.model, {
+          ...this.modelSets,
+          ...this.modelConsts,
+          ...this.modelParamns,
+        });
+
+        // Penalidades sempre se opõem ao objetivo principal
+        // (ex: se MAX, penalidade é negativa; se MIN, penalidade é positiva)
+        const penaltyMultiplier =
+          this.objectiveFunction.type === "max" ? -1 : 1;
+
+        const finalPenaltyTerms = objectiveTerms.map((t) => ({
+          ...t,
+          coefficient: penaltyMultiplier * t.coefficient,
+          // coefficient: t.coefficient * constraint.penalty * penaltyMultiplier,
+        }));
+
+        this.model.addObjectiveTerms(finalPenaltyTerms);
+      }
+    }
+  }
+
+  /**
+   * Implementação da execução do solver
+   */
+  protected async runSolver(): Promise<HighsSolverResult> {
     const highs_settings = {
       // In node, locateFile is not needed
       // In the browser, point locateFile to the URL of the wasm file (see below)
@@ -236,8 +244,8 @@ export class MILP {
 
     const highs = await highs_promise;
 
-    const solution = highs.solve(this.model.toCplexLpFormat(), {});
-
-    return solution;
+    const lpString = this.model.toCplexLpFormat();
+    console.log(lpString);
+    return highs.solve(lpString, {});
   }
 }
