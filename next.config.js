4c9d21cda3c218b952c917a63e2a8a76ce3c44d8
José Bieco <jo_bieco@hotmail.com>
2026-03-20 09:44:15 -0300

    feat(next.config.js): next-intl nas configurações do next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const createNextIntlPlugin = require('next-intl/plugin');
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig = {
   // output: 'export',
   webpack: (config, { isServer }) => {
@@ -14,4 +18,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withNextIntl(nextConfig);
